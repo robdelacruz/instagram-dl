1a2b7674ddaa51f39adc02b61c92f40a2e9cf182
robdelacruz <robdelacruz@gmail.com>
2016-10-09 01:32:49 +0800

    Allow multiple urls in input.

--- instagram-dl.pl
+++ instagram-dl.pl
@@ -7,10 +7,10 @@
 ### instagram-dl
 ### Download image file from an instagram link.
 ### Run using:
-### instagram-dl <url> [output filename]
+### instagram-dl <url>...
 ###
 ### Ex.
-### ./instagram-dl https://www.instagram.com/p/BEZ0PHjtUhH/ output.jpg
+### ./instagram-dl https://www.instagram.com/p/BEZ0PHjtUhH/
 ###
 
 use strict;
@@ -27,52 +27,47 @@ if (
 	@ARGV == 0 ||
 	(@ARGV == 1 && ($ARGV[0] eq '-h' || $ARGV[0] eq '-help' || $ARGV[0] eq '-?'))
 ) {
-	print "\nUsage:\ninstagram-dl [-h] <url> [output filename]\n\n";
+	print "\nUsage:\ninstagram-dl [-h] <url>... \n\n";
 	exit 0;
 }
 
-my $url;
-my $image_filename;
-if (@ARGV > 0) {
-	$url = $ARGV[0];
-}
-if (@ARGV > 1) {
-	$image_filename = $ARGV[1];
+for (@ARGV) {
+	process_url($_);
 }
 
-# Download page markup
-print "Accessing $url... ";
-my $resp = $tiny->get($url);
-die "Unable to access $url" unless $resp->{success};
-print "Done.\n";
+sub process_url {
+	my $url = shift;
 
-#
-# Extract image url from the following tag in page markup:
-# <meta property="og:image" content="..." />
-#
-my $content = $resp->{content};
-$content =~ /<meta property="og:image" content="(.*?)" \/>/s;
-my $image_url = $1;
-die "No image found in $url" unless defined $image_url;
+	# Download page markup
+	print "Accessing $url... ";
+	my $resp = $tiny->get($url);
+	die "Unable to access $url" unless $resp->{success};
+	print "Done.\n";
 
-#
-# Use the internal instagram filename if no output filename was specified.
-#
-if (!defined $image_filename) {
+	#
+	# Extract image url from the following tag in page markup:
+	# <meta property="og:image" content="..." />
+	#
+	my $content = $resp->{content};
+	$content =~ /<meta property="og:image" content="(.*?)" \/>/s;
+	my $image_url = $1;
+	die "No image found in $url" unless defined $image_url;
+
 	$image_url =~ /\/([^\/]+?\.jpg)/;
-	$image_filename = $1;
+	my $image_filename = $1;
 	die "Can't determine image filename" unless defined $image_filename;
+
+	# Download image url and save to file.
+	print "Downloading file '$image_filename'... ";
+	$resp = $tiny->get($image_url);
+	die "Unable to access $image_url" unless $resp->{success};
+
+	open my $hout_image, '>', $image_filename
+		or die "Unable to write to $image_filename: $!\n";
+	binmode($hout_image);
+	print $hout_image scalar $resp->{content};
+	close $hout_image;
+	print "Done.\n";
 }
 
-# Download image url and save to file.
-print "Downloading file '$image_filename'... ";
-$resp = $tiny->get($image_url);
-die "Unable to access $image_url" unless $resp->{success};
-
-open my $hout_image, '>', $image_filename
-	or die "Unable to write to $image_filename: $!\n";
-binmode($hout_image);
-print $hout_image scalar $resp->{content};
-close $hout_image;
-print "Done.\n";
 
